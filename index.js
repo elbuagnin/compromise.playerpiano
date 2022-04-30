222d48a3debf189f8c3465113b6b2235f6faa213
Elbuagnin <elbuagnin@gmail.com>
2022-04-30 14:47:11 -0500

    update names

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import initialize from "./initialize.js";
 import sequencer from "./sequencer.js";
 
-export const disambiguationPlugin = {
- api: (View) => {
-   View.prototype.disambiguate = function () {
-    initialize(this);
-    sequencer(this);
-    return this;
-   }
- }
+export const pianoplayerPlugin = {
+  api: View => {
+    View.prototype.sequence = function() {
+      initialize(this);
+      sequencer(this);
+      return this;
+    };
+  }
 };
