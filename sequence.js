32263a192cf066495aab1c92c15bc91b1bb82283
Elbuagnin <elbuagnin@gmail.com>
2022-05-01 12:53:00 -0500

    import path

--- sequence.js
+++ sequence.js
@@ -1,5 +1,6 @@
 import * as mfs from "./lib/filesystem.js";
 import * as dirs from "./data-file-structure.js";
+import path from "path";
 
 const file = path.join(dirs.baseData, "sequence.json");
 const sequence = mfs.loadJSONFile(file, "array");
