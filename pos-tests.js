86f578d75b4abf4cc227b03c26660ea9f47f91a3
Elbuagnin <elbuagnin@gmail.com>
2022-05-01 13:05:07 -0500

    more dirs.

--- pos-tests.js
+++ pos-tests.js
@@ -1,6 +1,6 @@
 import * as mfs from "./lib/filesystem.js";
 import * as dirs from "./data-file-structure.js";
 
-const posTests = mfs.loadJSONDir(classifierPatterns, true);
+const posTests = mfs.loadJSONDir(dirs.classifierPatterns, true);
 
 export default posTests;
