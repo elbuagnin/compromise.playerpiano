726a61ca2a28ac6dfdc5cf96922ccbabfcdce988
Elbuagnin <elbuagnin@gmail.com>
2022-05-01 12:25:07 -0500

    error fix

--- data-file-structure.js
+++ data-file-structure.js
@@ -1,13 +1,20 @@
-import path from ('path');
+import path from "path";
 
 const baseDataPath = "./sequencing-data/";
 const classifiersPath = path.join(baseDataPath, "classifiers");
 const classifierKeysPath = path.join(classifiersPath, "classification-keys");
-const classifierByTermsPath = path.join(classifiersPath, "classifications-by-terms");
-const classifierByClassificationsPath = path.join(classifiersPath,
+const classifierByTermsPath = path.join(
+  classifiersPath,
+  "classifications-by-terms"
+);
+const classifierByClassificationsPath = path.join(
+  classifiersPath,
   "terms-by-classifications"
 );
-const classifierPatternsPath = path.join(classifiersPath, "classifier-patterns");
+const classifierPatternsPath = path.join(
+  classifiersPath,
+  "classifier-patterns"
+);
 const processorsPath = path.join(baseDataPath, "doc-processors");
 const subSequencesPath = path.join(baseDataPath, "sub-sequences");
 const tagPatternsPath = path.join(baseDataPath, "tag-by-patterns");
