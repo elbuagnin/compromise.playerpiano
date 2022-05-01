ead60045fd70ee9538e5cf057f8e75d66dcb27de
Elbuagnin <elbuagnin@gmail.com>
2022-05-01 15:07:41 -0500

    missing path info

--- pos-tests.js
+++ pos-tests.js
@@ -1,6 +1,8 @@
 import * as mfs from "./lib/filesystem.js";
 import * as dirs from "./data-file-structure.js";
+import path from "path";
 
+const posTestsPath = path.join(dirs.classifierPatterns, "pos-tests");
 const posTests = mfs.loadJSONDir(dirs.classifierPatterns, true);
 
 export default posTests;
