01bfacc5ef859aa61aead2415a6376cf9426a6a3
Elbuagnin <elbuagnin@gmail.com>
2022-05-01 23:13:26 -0500

    dot

--- processor.js
+++ processor.js
@@ -46,7 +46,7 @@ export default function process(doc, parsingData) {
   }
 
   const { process } = parsingData;
-  const processPath = path.join("./", dirs.processors, process + ".js");
+  const processPath = path.join(".", dirs.processors, process + ".js");
   console.log(processPath);
   const processor = import(processPath);
   processor(doc);
