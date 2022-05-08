5902434aee5492e032b6f73bd4406f49e5db6b30
Charles Fewlass <elbuagnin@gmail.com>
2022-05-08 09:08:09 -0500

    refactored

--- logger.js
+++ logger.js
@@ -3,82 +3,98 @@ import { pianoOptions } from "./config.js";
 export default function logger(output, format = false, label = false) {
   const { verbose } = pianoOptions;
   let banner = "";
+
   if (verbose === true) {
-    // Format the label &|| output.
-    if (format && label) {
-      switch (format) {
-        case "label":
-          label = label[0].toUpperCase() + label.substring(1);
-          break;
-        case "header":
-          label = label.toUpperCase();
-          break;
-        case "title":
-          label = label.toUpperCase();
-          banner = Object.keys(label).forEach((letter) => {
-            banner += "*";
-          });
-          break;
-        default:
-          break;
-      }
-    } else {
-      switch (format && typeof output === "string") {
-        case "header":
-          label = output.toUpperCase();
-          output = "";
-          break;
-        case "title":
-          label = output.toUpperCase();
-          banner = Object.keys(label).forEach((letter) => {
-            banner += "*";
-          });
-          output = "";
-          break;
-        default:
-          break;
-      }
-    }
-
-    // Send output.
-    if (typeof output === "object") {
-      if (format === "header" || format === "title") {
-        console.log("\n");
-      }
-
+    let outputType = typeof output;
+    if (outputType === "object") {
       if (
         output.constructor.name === "View" ||
         output.constructor.name === "Sentences"
       ) {
-        // doc Object
-        if (label && format === "header") {
-          console.log(label);
-        }
+        outputType = "doc";
+      }
+    }
 
+    if (format === false) {
+      format = "formatless";
+    }
+    let layout = label ? "label" : "nolabel";
+    layout += "#" + format + "#" + outputType;
+
+    switch (layout) {
+      case "nolabel#title#string":
+        banner = banner.padStart(output.length, "*");
+        output = output.toUpperCase();
+        console.log("\n" + banner);
+        console.log(output);
+        break;
+      case "nolabel#subtitle#string":
+        banner = banner.padStart(output.length, "-");
+        output = output.toUpperCase();
+        console.log("\n" + banner);
+        console.log(output);
+        break;
+      case "nolabel#header#string":
+        output = output.toUpperCase();
+        console.log("\n" + output);
+        break;
+      case "nolabel#formatless#string":
+        console.log(output);
+      case "label#title#object":
+        banner = banner.padStart(label.length, "*");
+        label = label.toUpperCase();
+        console.log("\n" + banner);
+        console.log(label);
+        console.log(output);
+        break;
+      case "label#subtitle#object":
+        banner = banner.padStart(label.length, "-");
+        label = label.toUpperCase();
+        console.log("\n" + banner);
+        console.log(label);
+        console.log(output);
+        break;
+      case "label#header#object":
+        label = label.toUpperCase();
+        console.log("\n" + label);
+        console.log(output);
+        break;
+      case "label#label#object":
+        label = label[0].toUpperCase() + label.substring(1);
+        console.log(label + output);
+        break;
+      case "nolabel#formatless#object":
+        console.log(output);
+        break;
+      case "label#title#doc":
+        banner = banner.padStart(label.length, "*");
+        label = label.toUpperCase();
+        console.log("\n" + banner);
+        console.log(label);
         output.debug();
-      } else {
-        // other Object
-        if (label && format === "label") {
-          console.log(label + ":\n" + output);
-        } else {
-          console.log(output);
-        }
-      }
-    } else {
-      // Straight text
-      switch (format) {
-        case "header":
-          if (label) {
-            console.log(label + ": " + output);
-          }
-          break;
-        case "title":
-          console.log(banner);
-          console.log(label);
-          break;
-        default:
-          break;
-      }
+        break;
+      case "label#subtitle#doc":
+        banner = banner.padStart(label.length, "-");
+        label = label.toUpperCase();
+        console.log("\n" + banner);
+        console.log(label);
+        output.debug();
+        break;
+      case "label#header#doc":
+        label = label.toUpperCase();
+        console.log("\n" + label);
+        output.debug();
+        break;
+      case "label#label#doc":
+        label = label[0].toUpperCase() + label.substring(1);
+        console.log("\n" + label);
+        output.debug();
+        break;
+      case "nolabel#formatless#doc":
+        output.debug();
+        break;
+      default:
+        break;
     }
   }
 }
