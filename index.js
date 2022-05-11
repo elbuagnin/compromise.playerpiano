17b01e7ee84593f310c84f116cc6d76b6ad7f99c
Charles Fewlass <elbuagnin@gmail.com>
2022-05-10 19:03:26 -0500

    dev output for changes from index.js

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-import { setOptions } from "./config.js";
+import { setOptions, pianoOptions } from "./config.js";
+import devLogger from "./lib/dev-logger.js";
+import { surfaceCopy, equivalentDocs } from "./lib/doc-helpers.js";
 import initialize from "./initialize.js";
 import sequencer from "./workers/sequencer.js";
 
 const pianoplayer = {
   api: (View) => {
     View.prototype.sequence = function () {
+      function snapshot(doc, format = "formatless", message = "") {
+        if (pianoOptions.verbose !== "none") {
+          devLogger("changes", doc, format, message);
+          return surfaceCopy(doc);
+        } else {
+          return false;
+        }
+      }
+
       if (arguments.length > 0) {
         setOptions(arguments);
       }
 
       initialize(this);
+      const entryDoc = snapshot(this, "title", "Entry Document");
       sequencer(this);
+      const exitDoc = snapshot(this, "title", "Exit Document");
+
+      if (entryDoc !== false && exitDoc !== false) {
+        if (!equivalentDocs(entryDoc, exitDoc)) {
+          devLogger("changes", "There are changes to the Document.", "header");
+        }
+      }
+
       return this;
     };
   },
